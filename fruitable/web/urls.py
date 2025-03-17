ff0290cce18a9a6545d1c8cbf5c2a3748b59ec5d
MuliJob <munyoki912@gmail.com>
2025-03-17 16:12:34 +0300

    added shop page

--- fruitable/web/urls.py
+++ fruitable/web/urls.py
@@ -19,4 +19,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name="home"),
+    path('shop', views.shop_page, name="shop"),
 ]
--- fruitable/web/views.py
+++ fruitable/web/views.py
@@ -5,4 +5,21 @@ from django.shortcuts import render
 
 def home_page(request):
     """Home page function"""
-    return render(request, 'index.html')
+    title = 'FRUITABLES - HOME'
+
+    context = {
+        "title": title
+    }
+
+    return render(request, 'index.html', context)
+
+
+def shop_page(request):
+    """Shop page function"""
+    title = 'FRUITABLES - SHOP'
+
+    context = {
+        "title": title
+    }
+
+    return render(request, 'shop.html', context)
