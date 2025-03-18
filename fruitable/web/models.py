552e6e0d1ada701fe2a18a2cf80ff3654c725d66
MuliJob <munyoki912@gmail.com>
2025-03-18 17:59:42 +0300

    created product models

--- fruitable/web/models.py
+++ fruitable/web/models.py
@@ -1,3 +1,29 @@
+"""
+  APPLICATION MODELS
+"""
 from django.db import models
 
-# Create your models here.
+
+PRODUCT_CATEGORIES = (
+        ('APPLES', 'Apples'),
+        ('ORANGES', 'Oranges'),
+        ('STRAWBERRY', 'Strawberry'),
+        ('BANANA', 'Banana'),
+        ('PUMPKIN', 'Pumpkin'),
+        ('VEGETABLE', 'Vegetable')
+)
+class Product(models.Model):
+    """Products table"""
+    product_name = models.CharField(max_length=255, blank=True, null=True)
+    product_image = models.ImageField(blank=True, null=True, upload_to='products/')
+    product_category = models.CharField(max_length=255,
+                                          blank=True,
+                                          null=True,
+                                          choices=PRODUCT_CATEGORIES
+                                        )
+    product_description = models.TextField(blank=True, null=True)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.product_name
