85d47013300d938920046ea252925ebc285d75bb
MuliJob <munyoki912@gmail.com>
2025-03-19 16:26:02 +0300

    adding and displaying more fields to the database

--- fruitable/web/models.py
+++ fruitable/web/models.py
@@ -6,12 +6,8 @@ from django.db import models
 
 
 PRODUCT_CATEGORIES = (
-        ('Apples', 'Apples'),
-        ('Oranges', 'Oranges'),
-        ('Strawberries', 'Strawberries'),
-        ('Bananas', 'Bananas'),
-        ('Pumpkins', 'Pumpkins'),
-        ('Vegetables', 'Vegetables')
+        ('Vegetable', 'Vegetable'),
+        ('Fruits', 'Fruits')
 )
 class Product(models.Model):
     """Products table"""
@@ -19,12 +15,18 @@ class Product(models.Model):
     product_name = models.CharField(max_length=255, blank=True, null=True)
     product_image = models.ImageField(blank=True, null=True, upload_to='products/')
     product_price = models.CharField(max_length=10, blank=True, null=True)
+    product_weight = models.CharField(max_length=10, blank=True, null=True)
+    product_origin = models.CharField(max_length=50, blank=True, null=True)
+    product_quality = models.CharField(max_length=20, blank=True, null=True)
+    product_check = models.CharField(max_length=20, blank=True, null=True, default='Healthy')
+    product_min_weight = models.CharField(max_length=10, blank=True, null=True)
     product_category = models.CharField(max_length=255,
                                           blank=True,
                                           null=True,
                                           choices=PRODUCT_CATEGORIES
                                         )
     product_description = models.TextField(blank=True, null=True)
+    product_detail_description = models.TextField(blank=True, null=True)
 
     objects = models.Manager()
 
