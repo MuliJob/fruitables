ac4538c894330deee3570352ec6684c1f304d941
MuliJob <munyoki912@gmail.com>
2025-03-31 13:45:21 +0300

    Created function to allow admin to create an email template to send to subscribers

--- fruitable/web/admin.py
+++ fruitable/web/admin.py
@@ -1,8 +1,49 @@
 from django.contrib import admin
+from django import forms
+from django.conf import settings
+from django.core.mail import send_mail
+from django_ckeditor_5.fields import CKEditor5Widget
 
-from .models import Cart, CartItem, Product
+from .models import Cart, CartItem, Product, Subscriber, EmailTemplate
+
+
+
+class EmailTemplateAdminForm(forms.ModelForm):
+    """Email Template Admin"""
+    class Meta:
+        """Class meta"""
+        model = EmailTemplate
+        fields = '__all__'
+        widgets = {
+            'message': CKEditor5Widget(),
+        }
+
+
+class EmailTemplateAdmin(admin.ModelAdmin):
+    """Admin template"""
+    form = EmailTemplateAdminForm
+
+    def save_model(self, request, obj, form, change):
+        """Saving details to model"""
+        super().save_model(request, obj, form, change)
+        subject = obj.subject
+        html_message = obj.message
+
+        recipients = [subscriber.email for subscriber in obj.recipients.all()]
+        from_email = settings.EMAIL_HOST_USER
+        send_mail(
+            subject,
+            "",
+            from_email,
+            recipients,
+            fail_silently=False,
+            html_message=html_message
+        )
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(CartItem)
 admin.site.register(Cart)
+admin.site.register(Subscriber)
+
+admin.site.register(EmailTemplate,  EmailTemplateAdmin)
