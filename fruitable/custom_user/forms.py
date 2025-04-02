6df0d274b66ab6cb1888d089fd7656ca9588ed44
MuliJob <munyoki912@gmail.com>
2025-04-02 18:06:22 +0300

    updated login and register form

--- fruitable/custom_user/forms.py
+++ fruitable/custom_user/forms.py
@@ -10,6 +10,23 @@ from .models import User
 
 class CreateUserForm(UserCreationForm):
     """Register form"""
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={'class': 'form-control', 'placeholder': 'Enter your email'}
+        )
+    )
+
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control', 'placeholder': 'Enter your password'}
+        )
+    )
+
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control', 'placeholder': 'Confirm your password'}
+        )
+    )
     class Meta:
         """Class meta"""
         model = User
